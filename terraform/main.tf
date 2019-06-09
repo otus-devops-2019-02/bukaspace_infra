87848f8c9ba4b6d955a571ba305320618f49bd92
buka <buka@otuspoc.mshome.net>
2019-06-09 11:19:20 +0000

    Cleaned up

---
+++ terraform/main.tf
@@ -0,0 +1,14 @@
+terraform {
+   # Версия terraform
+   required_version = "0.11.11"
+ } 
+ 
+provider "google" {
+   # Версия провайдера
+   version = "2.0.0" 
+   # ID проекта
+   project = "rugged-research-235212" 
+ 
+  region = "europe-west-1"
+ }
+
