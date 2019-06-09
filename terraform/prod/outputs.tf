eb47705dd27c21653ae2b93229a64946bd8383d9
buka <buka@otuspoc.mshome.net>
2019-06-09 12:47:28 +0000

    Done hw 9

---
+++ terraform/prod/outputs.tf
@@ -0,0 +1,7 @@
+output "app_external_ip" {
+
+  value = "${module.app.app_external_ip}"
+
+}
+
+
---
+++ terraform/prod/variables.tf
@@ -0,0 +1,83 @@
+
+
+variable project {
+
+  description = "Project ID"
+
+}
+
+
+
+variable region {
+
+  description = "Region"
+
+
+
+  # Значение по умолчанию
+
+  default = "europe-north1-b"
+
+}
+
+
+
+variable zone {
+
+  description = "Region"
+
+
+
+  # Значение по умолчанию
+
+  default = "europe-north1-b"
+
+}
+
+
+
+variable public_key_path {
+
+  # Описание переменной
+
+  description = "Path to the public key used for ssh access"
+
+}
+
+
+
+variable disk_image {
+
+  description = "Disk image"
+
+}
+
+
+
+variable db_disk_image {
+
+  description = "Disk image for reddit db"
+
+  default     = "reddit-db-base"
+
+}
+
+
+
+variable app_disk_image {
+
+  description = "Disk image for reddit app"
+
+  default     = "reddit-app-base"
+
+}
+
+
+
+variable private_key_path {
+
+  # Описание переменной
+
+  description = "Path to the public key used for ssh access"
+
+}
